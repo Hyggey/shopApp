87c9493cda8d72eb5df4c50b6dddbc1601d43896
zhangzhen1897 <zhangzhen1897@gmail.com>
2019-11-07 17:31:48 +0800

    123

--- src/views/shopPage/pages/shopInfo.vue
+++ src/views/shopPage/pages/shopInfo.vue
@@ -1,6 +1,19 @@
 <template>
-    <div>
-        shopinfo
+    <div class="shopInfoContainer">
+        <div class="shopInfo">
+            <div class="module_msg">
+                <h3>配送信息</h3>
+                <div class="msg">
+                    <span>硅谷专送</span>
+                    <span>由商家配送提供配送，约28分钟送达，距离1000m</span>
+                </div>
+                <p>配送费￥4</p>
+            </div>
+            <div class="liver"></div>
+            <div>
+                <h1>活动与服务</h1>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +24,27 @@ export default {
 </script>
 
 <style lang="stylus">
-    
+    .shopInfoContainer
+        .shopInfo
+            .module_msg
+                padding 16px 14px 14px
+                .msg
+                    margin-top 13px
+                    span:first-child
+                        background #0097ff
+                        color #fff
+                        font-size 12px
+                        border-radius 4px
+                        width 55px
+                        display inline-block
+                        padding 3px 0
+                        text-align center
+                    span:last-child 
+                        font-size 14px
+                        color #666
+                p 
+                    margin-top 4px
+                    font-size 14px
+                    color #666
+                    
 </style>
